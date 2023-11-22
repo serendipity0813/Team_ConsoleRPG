69c9ef7959a8e999dde65a7fef471f0d946a8ab1
colarrot <gunho7935@gmail.com>
2023-11-22 18:37:03 +0900

    bgm

--- Team_ConsoleRPG/ConsoleRPG.cs
+++ Team_ConsoleRPG/ConsoleRPG.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using NAudio.Wave;
 using System.Collections.Generic;
 using System.IO;
 using Team_ConsoleRPG;
@@ -12,13 +13,35 @@ namespace ConsoleRPG
 
         public static void Main(string[] args)
         {
-            //게임시작점 - 아이템, 플레이어, 몬스터 데이터 세팅
-            DataManager.DataSetting();
+            string filePath = "C:\\Users\\gunho\\Desktop\\music\\bgm.mp3"; // MP3 파일 경로를 지정하세요.
 
-            if(!Player.GetInst.Load())
-                StartScene.StartMain();
-            
-            GameManager.DisplayHome();      //게임메니저 호출하여 메인로비 호출
+            using (var mp3Reader = new Mp3FileReader(filePath))
+            {
+                // WaveChannel32를 사용하여 소리 크기 조절
+                var waveChannel = new WaveChannel32(mp3Reader);
+
+                using (var waveOut = new WaveOutEvent())
+                {
+                    waveOut.Init(waveChannel);
+
+                    // 소리 크기 조절
+                    waveChannel.Volume = 0.03f; // 0.5는 반으로 줄이는 예시입니다.
+
+                    waveOut.Play();
+                    //게임시작점 - 아이템, 플레이어, 몬스터 데이터 세팅
+                    DataManager.DataSetting();
+
+                    if (!Player.GetInst.Load())
+                        StartScene.StartMain();
+
+                    GameManager.DisplayHome();      //게임메니저 호출하여 메인로비 호출
+
+                    Console.WriteLine("음악 재생 중... (Press Enter to stop)");
+                    Console.ReadLine();
+
+                    waveOut.Stop();
+                }
+            }
         }
 
     }
